0b2031dd5a5afd491925d044ff4f9de3ebf90e1e
Stephen Zuniga <stephen.zuniga001@gmail.com>
2015-02-22 19:47:32 -0500

    Allow for options to be passed to sass

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 module.exports = function (file) {
     var sass = require('node-sass');
 
-    return sass.renderSync({
-        file : file,
-    }).stats.includedFiles;
+    // Default options to use for the sass compiler
+    var options = {};
+
+    // If a string is passed, assume it's a file
+    if (typeof file == 'string') {
+        options.file = file;
+    } else {
+        options = file;
+    }
+
+    // Return the imported files
+    return sass.renderSync(options).stats.includedFiles;
 };
